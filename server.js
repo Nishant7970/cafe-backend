a23c6432ce53d1df24980ab89549676f464c1a2c
Nishant7970 <nk7970922655@gmail.com>
2025-07-09 15:16:09 +0530

    update

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./Routes/userRoute.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(express.json());
+const dbuser = encodeURIComponent(process.env.DBUSER);
+const dbpass = encodeURIComponent(process.env.DBPASS);
 
 app.use("/api/users", userRouter);
 
-mongoose.connect("mongodb://127.0.0.1:27017/merncafe").then(() => {
+mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.5jgsi4d.mongodb.net/mern-cafe?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
   app.listen(8080, () => {
     console.log("Server started at 8080");
   });

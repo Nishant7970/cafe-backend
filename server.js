3947f0ad0f0692e7e97acf5f5e215498102f7d17
Nishant7970 <nk7970922655@gmail.com>
2025-07-09 16:10:58 +0530

    up

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./Routes/userRoute.js";
-import dotenv from "dotenv";
-dotenv.config();
+// import dotenv from "dotenv";
+// dotenv.config();
 
 const app = express();
 app.use(express.json());
-const dbuser = encodeURIComponent(process.env.DBUSER);
-const dbpass = encodeURIComponent(process.env.DBPASS);
+// const dbuser = encodeURIComponent(process.env.DBUSER);
+// const dbpass = encodeURIComponent(process.env.DBPASS);
 
 app.use("/api/users", userRouter);
 
-mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.5jgsi4d.mongodb.net/mern-cafe?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
-  app.listen(8080, () => {
-    console.log("Server started at 8080");
+// mongoose.connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.5jgsi4d.mongodb.net/mern-cafe?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
+//   app.listen(8080, () => {
+//     console.log("Server started at 8080");
+//   });
+// });
+mongoose.connect(`mongodb+srv://nk7970922655:SmJmHiU0S9UiCprp@cluster0.5jgsi4d.mongodb.net/mern-cafe2?retryWrites=true&w=majority&appName=Cluster0`)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(8080, () => {
+      console.log("Server started at 8080");
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection failed:", err.message);
   });
-});
+
 

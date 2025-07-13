d44d84e16280810e005d33efd4ca73923cb3c062
Nishant7970 <nk7970922655@gmail.com>
2025-07-13 11:13:30 +0530

    up

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ import cors from "cors";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin:["http://localhost:3000", 'https://nishcafe.netlify.app']
+}));
 // const dbuser = encodeURIComponent(process.env.DBUSER);
 // const dbpass = encodeURIComponent(process.env.DBPASS);
 
